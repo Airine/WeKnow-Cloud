f9f3db9f6dedad1cc555301883f8a593ebf79be4
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-22 14:13:09 +0800

    Implement the function of login. Storage the data of SID and PWD. Have not implemented the function of checking the SID and PWD.

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,215 +5,245 @@ var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 // var lifeData = require('./data/life/main.js');
 var fileData = require('../../utils/data.js')
 Page({
-	data: {
-		// 用户信息
-		userInfo: null,
-		avatarUrl: '/src/icon/loginDefault.png',
-		logged: false,
-		takeSession: false,
-		requestResult: '',
+  data: {
+    // 用户信息
+    userInfo: null,
+    avatarUrl: '/src/icon/loginDefault.png',
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    SID: '',
+    PWD: null,
 
-    	// 显示相关
-		fixedH: 114,
-		fixedPercent: 85,
-		StatusBar: app.globalData.StatusBar,
-		CustomBar: app.globalData.CustomBar,
-		WindowH: app.globalData.WindowH,
-		WindowW: app.globalData.WindowW,
-		// ScreenH: app.globalData.ScreenH,
-		// tabHeight: app.globalData.WindowH - app.globalData.CustomBar,
-		// pageHeight: app.globalData.WindowH + app.globalData.CustomBar,
-		topNum: 0,
-		tabTop: false,
-		inputShowed: false,
-		inputVal: "",
-		swiperOn: false,
-		movies:[
-			{ url: '../../src/img/1.jpeg' },
-			{ url: '../../src/img/2.jpeg' },
-			{ url: '../../src/img/3.jpeg' },
-			{ url: '../../src/img/4.jpeg' }
-		],
-		// tab 相关
-		tabs: [],
-		// subs: [],
+    // 显示相关
+    fixedH: 114,
+    fixedPercent: 85,
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    WindowH: app.globalData.WindowH,
+    WindowW: app.globalData.WindowW,
+    // ScreenH: app.globalData.ScreenH,
+    // tabHeight: app.globalData.WindowH - app.globalData.CustomBar,
+    // pageHeight: app.globalData.WindowH + app.globalData.CustomBar,
+    topNum: 0,
+    tabTop: false,
+    inputShowed: false,
+    inputVal: "",
+    swiperOn: false,
+    movies: [{
+        url: '../../src/img/1.jpeg'
+      },
+      {
+        url: '../../src/img/2.jpeg'
+      },
+      {
+        url: '../../src/img/3.jpeg'
+      },
+      {
+        url: '../../src/img/4.jpeg'
+      }
+    ],
+    // tab 相关
+    tabs: [],
+    // subs: [],
 
-		activeIndex: 0,
-		sliderOffset: 0,
-		sliderLeft: 0,
-		// 选项相关
-		// titles: fileData.titleData(),
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    // 选项相关
+    // titles: fileData.titleData(),
 
-		posts: [],
+    posts: [],
 
-		// scrollable: false,
-		content: false,
+    // scrollable: false,
+    content: false,
 
-		isLoadContent: false,
-		isLoadErr: false,
-		appear: false,
-	},
-	onLoad: function () {
-		var that = this;
-		var fixedHH = app.globalData.WindowW * 50 / 375 + app.globalData.CustomBar;
-		var percent = parseInt(100 - 100 * fixedHH / app.globalData.WindowH, 10);
-		// console.log(fixedHH);
-		// console.log(percent);
-		that.setData({
-			fixedH: fixedHH,
-			fixedPercent: percent
-		});
-		wx.request({
-			// url: "http://119.29.214.174/categories/",
-			url: "https://dev.citric-acid.com.cn/categories/",
-			method: "GET",
-			success: function (res) {
-				// console.log(res.data)
-				that.setData({
-					tabs: res.data
-				})
-				wx.getSystemInfo({
-					success: function (res) {
-						that.setData({
-							sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-							sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
-						});
-					}
-				});
-			},
-			fail: function (err) {
-				console.log(err)
-			}
-	  	});
+    isLoadContent: false,
+    isLoadErr: false,
+    appear: false,
+  },
+  onLoad: function() {
+    var that = this;
+    var fixedHH = app.globalData.WindowW * 50 / 375 + app.globalData.CustomBar;
+    var percent = parseInt(100 - 100 * fixedHH / app.globalData.WindowH, 10);
+    // console.log(fixedHH);
+    // console.log(percent);
+    that.setData({
+      fixedH: fixedHH,
+      fixedPercent: percent
+    });
+    wx.request({
+      // url: "http://119.29.214.174/categories/",
+      url: "https://dev.citric-acid.com.cn/categories/",
+      method: "GET",
+      success: function(res) {
+        // console.log(res.data)
+        that.setData({
+          tabs: res.data
+        })
+        wx.getSystemInfo({
+          success: function(res) {
+            that.setData({
+              sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+              sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+            });
+          }
+        });
+      },
+      fail: function(err) {
+        console.log(err)
+      }
+    });
 
-		// 接口更新，禁止载入时要求授权
-		// 查看是否授权
-		wx.getSetting({
-		success: res => {
-			if (res.authSetting['scope.userInfo']) {
-				// 已经授权，加载global全局变量
-				this.setData({
-					logged: true,
-					avatarUrl: app.globalData.avatarUrl,
-					userInfo: app.globalData.userInfo,
-				});
-				// console.log('userInfo', res.userInfo)有bug
-			} 
+    // 接口更新，禁止载入时要求授权
+    // 查看是否授权
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，加载global全局变量
+          this.setData({
+            logged: true,
+            avatarUrl: app.globalData.avatarUrl,
+            userInfo: app.globalData.userInfo,
+          });
+          // console.log('userInfo', res.userInfo)有bug
+        }
 
-			// getUserInfo已废弃
-			wx.getUserInfo({
-			success: res => {
-				this.setData({
-				avatarUrl: res.userInfo.avatarUrl,
-				userInfo: res.userInfo,
-				});
-				// console.log('userInfo', res.userInfo)
-				app.globalData.avatarUrl = res.userInfo.avatarUrl;
-				app.globalData.userInfo = res.userInfo;
+        // getUserInfo已废弃
+        wx.getUserInfo({
+          success: res => {
+            this.setData({
+              avatarUrl: res.userInfo.avatarUrl,
+              userInfo: res.userInfo,
+            });
+            // console.log('userInfo', res.userInfo)
+            app.globalData.avatarUrl = res.userInfo.avatarUrl;
+            app.globalData.userInfo = res.userInfo;
 
-			}
-			});
-			
-			
-		}
-    	});
-		console.log("load one time")
-		this._observer = wx.createIntersectionObserver(this)
-		this._observer
-			.relativeTo('.scroll-main')
-			.observe('.swiper', (res) => {
-				console.log(res);
-				this.setData({
-					tabTop: res.intersectionRatio > 0
-				})
-			})
-	},
+          }
+        });
 
-	tabClick: function (e) {
-		var that = this;
-		var tempt = this.data.content;
-		this.setData({
-			sliderOffset: e.currentTarget.offsetLeft,
-			activeIndex: e.currentTarget.id,
-		});
-		if (tempt){
-			that.setData({
-				topNum: this.data.topNum = 0,
-				swiperOn: false,
-				content:false
-			})
-		}
-	},
-	onTapTitle: function(e) {
-		var that = this;
-		var _data = e.currentTarget.dataset;
-		// console.log('Now tap title:');
-		// console.log(_data);
-		this.setData({
-			content: true
-		});
-		wx.request({
-			url: "https://dev.citric-acid.com.cn/posts",
-			method: "GET",
-			data: {"subcategory": _data.title},
-			success: function (res) {
-				// console.log(res.data)
-				that.setData({
-					posts: res.data,
-					isLoadErr: false
-				})
-			},
-			fail: function (err) {
-				// console.log(err)
-				that.setData({
-					isLoadErr: true
-				});
-			},
-			complete: function(res) {
-				// console.log(res)
-				that.setData({
-					isLoadContent: true
-				});
-			}
-		});
-		// wx.navigateTo() -> according to the data.content
-	},
-	tapPost: function (e) {
-		var _data = e.currentTarget.dataset;
-		console.log(_data);
-		wx.request({
-			url: "https://dev.citric-acid.com.cn/posts/"+_data.id,
-			method: "GET",
-			success: function (res) {
-				// console.log(res.data);
-				// console.log(res.data.content);
-				app.globalData.markdown = res.data.content;
-				// console.log(app.globalData.markdown)
-				// app.globalData.markdown = res.data.content;
-				wx.navigateTo({
-					url: '../content/content',
-					success: function(res) {},
-					fail: function(res) {},
-					complete: function(res) {},
-				})
-			},
-			fail: function (err) {
-				console.log(err)
-			}
 
-		});
-	},
-	tapBack: function (e) {
-		this.setData({
-			content: false,
-			isLoadContent: false,
-			posts: [],
-		});
-	},
+      }
+    });
+    console.log("load one time")
+    this._observer = wx.createIntersectionObserver(this)
+    this._observer
+      .relativeTo('.scroll-main')
+      .observe('.swiper', (res) => {
+        console.log(res);
+        this.setData({
+          tabTop: res.intersectionRatio > 0
+        })
+      })
+
+    wx.getStorage({
+      key: 'SID',
+      success: function(res) {
+        console.log(res.data)
+        this.setData({
+          SID: res.data
+        })
+      },
+    })
+    wx.getStorage({
+      key: 'PWD',
+      success: function(res) {
+        console.log(res.data)
+        this.setData({
+          PWD: res.data
+        })
+      },
+    })
+  },
+
+  tabClick: function(e) {
+    var that = this;
+    var tempt = this.data.content;
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id,
+    });
+    if (tempt) {
+      that.setData({
+        topNum: this.data.topNum = 0,
+        swiperOn: false,
+        content: false
+      })
+    }
+  },
+  onTapTitle: function(e) {
+    var that = this;
+    var _data = e.currentTarget.dataset;
+    // console.log('Now tap title:');
+    // console.log(_data);
+    this.setData({
+      content: true
+    });
+    wx.request({
+      url: "https://dev.citric-acid.com.cn/posts",
+      method: "GET",
+      data: {
+        "subcategory": _data.title
+      },
+      success: function(res) {
+        // console.log(res.data)
+        that.setData({
+          posts: res.data,
+          isLoadErr: false
+        })
+      },
+      fail: function(err) {
+        // console.log(err)
+        that.setData({
+          isLoadErr: true
+        });
+      },
+      complete: function(res) {
+        // console.log(res)
+        that.setData({
+          isLoadContent: true
+        });
+      }
+    });
+    // wx.navigateTo() -> according to the data.content
+  },
+  tapPost: function(e) {
+    var _data = e.currentTarget.dataset;
+    console.log(_data);
+    wx.request({
+      url: "https://dev.citric-acid.com.cn/posts/" + _data.id,
+      method: "GET",
+      success: function(res) {
+        // console.log(res.data);
+        // console.log(res.data.content);
+        app.globalData.markdown = res.data.content;
+        // console.log(app.globalData.markdown)
+        // app.globalData.markdown = res.data.content;
+        wx.navigateTo({
+          url: '../content/content',
+          success: function(res) {},
+          fail: function(res) {},
+          complete: function(res) {},
+        })
+      },
+      fail: function(err) {
+        console.log(err)
+      }
+
+    });
+  },
+  tapBack: function(e) {
+    this.setData({
+      content: false,
+      isLoadContent: false,
+      posts: [],
+    });
+  },
 
   // 登录相关函数
   // 获取用户信息
-  onGetUserInfo: function (e) {
+  onGetUserInfo: function(e) {
     if (!this.logged && e.detail.userInfo) {
       this.setData({
         logged: true,
@@ -222,8 +252,8 @@ Page({
       });
       // 全局变量赋值
       app.globalData.logged = true,
-      app.globalData.avatarUrl= e.detail.userInfo.avatarUrl,
-      app.globalData.userInfo = e.detail.userInfo
+        app.globalData.avatarUrl = e.detail.userInfo.avatarUrl,
+        app.globalData.userInfo = e.detail.userInfo
       console.log('user info', e.detail.userInfo);
       // 调用云函数,存储openid
       this.onGetOpenid();
@@ -231,7 +261,7 @@ Page({
   },
 
   // 调用云函数
-  onGetOpenid: function () {
+  onGetOpenid: function() {
     wx.cloud.callFunction({
       name: 'login',
       data: {},
@@ -252,7 +282,7 @@ Page({
       }
     })
   },
-	onUnload() {
-		if (this._observer) this._observer.disconnect()
-	}
+  onUnload() {
+    if (this._observer) this._observer.disconnect()
+  }
 });
--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -20,12 +20,14 @@ Page({
     CustomBar: app.globalData.CustomBar,
     WindowH: app.globalData.WindowH,
     WindowW: app.globalData.WindowW,
+    SID: '',
+    PWD: null
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // var that = this;
     // var fixedHH = app.globalData.WindowW * 50 / 375 + app.globalData.CustomBar;
     // var percent = parseInt(100 - 100 * fixedHH / app.globalData.WindowH, 10);
@@ -40,49 +42,88 @@ Page({
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  login() {
+    if (this.data.SID && this.data.PWD) {
+      wx.switchTab({
+        url: '../index/index',
+      })
+      wx.setStorage({
+        key: 'SID',
+        data: this.data.SID,
+      })
+      wx.setStorage({
+        key: 'PWD',
+        data: this.data.PWD,
+      })
+    }else{
+      wx.showModal({
+        title: 'Info needed',
+        content: 'Please input your SID and PWD',
+        success: function(res){
+          if(res.confirm){
+            console.log("用户点击确定")
+          }
+        }
+      })
+      
+    }
+  },
+
+  inputID: function(e) {
+    this.setData({
+      SID: e.detail.value
+    })
+  },
+
+  inputPWD: function(e) {
+    this.setData({
+      PWD: e.detail.value
+    })
   }
 })
