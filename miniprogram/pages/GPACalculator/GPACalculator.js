f34163e05d2e43d4b041ac5fabd869aea2022dce
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-25 16:15:30 +0800

    Upload component of filter

---
+++ miniprogram/pages/GPACalculator/GPACalculator.js
@@ -0,0 +1,101 @@
+// miniprogram/pages/GPACalculator/GPACalculator.js
+
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    fixedH: 114,
+    fixedPercent: 85,
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    WindowH: app.globalData.WindowH,
+    WindowW: app.globalData.WindowW,
+    SID: '',
+    PWD: null,
+    Term: [],
+
+
+    cityCinemaInfo: {}, //城市影院信息
+    isShow: false,
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function() {
+    this.setData({
+      SID: app.globalData.SID,
+      PWD: app.globalData.PWD,
+      Term: app.globalData.Term
+    })
+
+    console.log(app.globalData.SID)
+    console.log(app.globalData.PWD)
+    console.log(app.globalData.Term)
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function() {
+
+  },
+
+
+  toggleShow(e) {
+    const item = e.detail.item
+    this.setData({
+      isShow: item !== -1
+    })
+  },
+
+  /**
+   * 用于切换选取的Term
+   */
+})
