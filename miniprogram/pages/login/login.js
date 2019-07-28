9aba599720ccdf9bd7d11dcd1a28e9adcedd2f33
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-28 16:05:02 +0800

    Process the return value from the post request in order to get the Object CourseInfo

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -21,7 +21,34 @@ Page({
     WindowH: app.globalData.WindowH,
     WindowW: app.globalData.WindowW,
     SID: '',
-    PWD: null
+    PWD: null,
+    originData: [], // url返回的数组数据，要做修改改成CourseInfo的形式
+    CourseInfo: {
+      "Term": {
+        "name": "学期",
+        "subItems": [{
+          "count": 0,
+          "id": -1,
+          "name": "全部"
+        }]
+      },
+      "Category": {
+        "name": "GPA类型",
+        "subItems": [{
+          "count": 0,
+          "id": -1,
+          "name": "全部"
+        }]
+      },
+      "Level": {
+        "name": "等级",
+        "subItems": [{
+          "count": 0,
+          "id": -1,
+          "name": "全部"
+        }]
+      }
+    }
   },
 
   /**
@@ -117,8 +144,93 @@ Page({
           } else {
             app.globalData.SID = that.data.SID
             app.globalData.PWD = that.data.PWD
+            that.setData({
+              originData: res.data
+            })
 
-            
+
+
+            // 这三个Map是用来给三种类型count
+            var termMap = new Map()
+            var cateMap = new Map()
+            var gradMap = new Map()
+
+            var originData = that.data.originData
+
+            // 先处理完Category的分类再去重
+            for (var index = 0; index < originData.length; index++) {
+              if (cateMap.has(originData[index].category)) {
+                cateMap.set(originData[index].category, cateMap.get(originData[index].category) + 1)
+              } else {
+                cateMap.set(originData[index].category, 1)
+              }
+            }
+
+            // 去重
+            var courseSet = new Set()
+            for (var index = 0; index < originData.length; index++) {
+              if (courseSet.has(originData[index].code)) {
+                originData.splice(index, 1)
+              } else {
+                courseSet.add(originData[index].code)
+              }
+            }
+
+
+            // 处理完category之后处理term和grade
+            for (var index = 0; index < originData.length; index++) {
+              if (termMap.has(originData[index].term)) {
+                termMap.set(originData[index].term, termMap.get(originData[index].term) + 1)
+              } else {
+                termMap.set(originData[index].term, 1)
+              }
+
+              if (gradMap.has(originData[index].grade)) {
+                gradMap.set(originData[index].grade, gradMap.get(originData[index].grade) + 1)
+              } else {
+                gradMap.set(originData[index].grade, 1)
+              }
+            }
+
+            // 更新CourseInfo
+            var mapIndex = 1
+            for (var [key, value] of termMap) {
+              that.data.CourseInfo.Term.subItems.push({
+                "count": value,
+                "id": mapIndex++,
+                "name": key
+              })
+            }
+            mapIndex = 1
+            for (var [key, value] of cateMap) {
+              that.data.CourseInfo.Category.subItems.push({
+                "count": value,
+                "id": mapIndex++,
+                "name": key
+              })
+            }
+            mapIndex = 1
+            for (var [key, value] of gradMap) {
+              that.data.CourseInfo.Level.subItems.push({
+                "count": value,
+                "id": mapIndex++,
+                "name": key
+              })
+            }
+
+            that.data.CourseInfo.Term.subItems[0].count = originData.length
+            that.data.CourseInfo.Category.subItems[0].count = originData.length
+            that.data.CourseInfo.Level.subItems[0].count = originData.length
+
+            app.globalData.CourseInfo = that.data.CourseInfo
+
+            // console.log(termMap)
+            // console.log(cateMap)
+            // console.log(gradMap)
+
+            console.log(that.data.CourseInfo)
+
+
 
             console.log("SID: " + that.data.SID);
             console.log("PWD: " + that.data.PWD);
@@ -131,8 +243,11 @@ Page({
             //   key: 'PWD',
             //   data: that.data.PWD,
             // })
-            wx.switchTab({
-              url: '../index/index',
+            // wx.switchTab({
+            //   url: '../index/index',
+            // })
+            wx.redirectTo({
+              url: '../GPACalculator/GPACalculator',
             })
           }
         },
